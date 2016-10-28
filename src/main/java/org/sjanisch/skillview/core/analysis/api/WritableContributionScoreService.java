f91a1885308669d74958429caa15fd403ed9ccc2
sebastian-janisch <sebastian.janisch@gmail.com>
2016-10-28 19:31:54 +0100

    fleshed out WritableContributionScoreService API

--- src/main/java/org/sjanisch/skillview/core/analysis/api/WritableContributionScoreService.java
+++ src/main/java/org/sjanisch/skillview/core/analysis/api/WritableContributionScoreService.java
@@ -2,8 +2,22 @@ package org.sjanisch.skillview.core.analysis.api;
 
 import java.util.Collection;
 
+/**
+ * Augments the {@link ContributionScoreService} by adding functionality to
+ * persist {@link DetailedContributionScore contribution scores}.
+ * <p>
+ * Implementors must retain thread safety.
+ * 
+ * @author sebastianjanisch
+ *
+ */
 public interface WritableContributionScoreService extends ContributionScoreService {
 
+	/**
+	 * 
+	 * @param contributionScores
+	 *            must not be {@code null}.
+	 */
 	void saveContributionScores(Collection<DetailedContributionScore> contributionScores);
 
 }
